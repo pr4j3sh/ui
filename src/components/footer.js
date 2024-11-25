b1fc8a0ade46be244081b29e662eb6424fd71d2c
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-11-25 14:29:45 +0530

    fixed footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,8 +2,13 @@ import { AUTHOR } from "../lib/consts";
 
 const component = `<footer>
         <p>
-<a href="${AUTHOR.URL}" target="_blank">${AUTHOR.USERNAME}</a>
+          &copy; <a href="${AUTHOR.URL}" target="_blank">${AUTHOR.USERNAME}</a>
         </p>
+        <article class="btns">
+          <a href="${AUTHOR.TWITTER}" target="_blank">twitter</a>
+          <span>/</span>
+          <a href="${AUTHOR.GITHUB}" target="_blank">github</a>
+        </article>
       </footer>`;
 
 const footer = document.getElementById("footer");
--- src/lib/consts.js
+++ src/lib/consts.js
@@ -10,4 +10,6 @@ export const AUTHOR = {
   USERNAME: "pr4j3sh",
   EMAIL: "prajesh.eleven118@gmail.com",
   URL: "https://pr4j3sh.vercel.app/",
+  TWITTER: "https://x.com/pr4j3sh",
+  GITHUB: "https://github.com/pr4j3sh",
 };
