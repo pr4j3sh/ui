70e4a9d00c806fa6b769b73e7a5f0ac203cba83e
pr4j3sh <prajesh.eleven118@gmail.com>
2025-04-18 05:47:47 +0530

    fix: example for card

--- src/components/components.js
+++ src/components/components.js
@@ -203,12 +203,12 @@ const items = [
       {
         title: "card",
         description: "Defines a card.",
-        code: `<code>&lt;article class="body"&gt;
+        code: `<code>&lt;article class="card"&gt;
   <code>&lt;img src="../public/banner.png" /&gt;</code>
   <code>&lt;article class="card-body"&gt;</code>
     <code>&lt;h6&gt;Card Title&lt;/h6&gt;</code>
     <code>&lt;p&gt;This is a simple description.&lt;/p&gt;</code>
-  <code>&lt;article/&gt;</code>
+  <code>&lt;/article&gt;</code>
 &lt;/article&gt;</code>`,
         usage: `<article class="card"><img src="${Banner}" /><article class="card-body"><h6>Card Title</h6><p>This is a simple description.</p></article></card>`,
       },
